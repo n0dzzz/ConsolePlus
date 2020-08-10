6483038816412f6a06d972c9837a8e3cc31d6e0f
GrandmaSocks <62735552+GrandmaSocks@users.noreply.github.com>
2020-08-10 09:09:14 -0700

    Added some functions

--- ConsolePlus/ConsolePlus/ConsolePlus.cpp
+++ ConsolePlus/ConsolePlus/ConsolePlus.cpp
@@ -16,14 +16,14 @@ namespace console
 	
 	void space()
 	{
-		printf("");
+		std::cout << "\n";
 	}
 
-	void typingText(std::string message)
+	void typingText(std::string message, int delay)
 	{
 		for (int i = 0; i < message.length(); ++i )
 		{ 
-			Sleep(300);
+			Sleep(delay);
 			std::cout << message[i];
 
 		}
@@ -50,12 +50,38 @@ namespace console
 		std::cout << var << std::endl;
 	}
 
+	void loadingText(std::string message, int delay)
+	{
+		std::cout << message;
+		
+		Sleep(delay);
+		std::cout << ".";
+
+		Sleep(delay);
+		std::cout << ".";
+
+		Sleep(delay);
+		std::cout << ". \n";
+	}
+
+	void print(std::string message)
+	{
+		std::cout << message;
+	}
+
+	void println(std::string message)
+	{
+		std::cout << message << std::endl;
+	}
+
+
+
+
 	/*void cin(std::string inputVar)
 	{
 		std::getline(std::cin, inputVar);
 	}
 	*/
-
 	/* void rainbowText(std::string message)
 	{
 		for (int i = 0; i < message.length(); ++i)
--- ConsolePlus/ConsolePlus/ConsolePlus.h
+++ ConsolePlus/ConsolePlus/ConsolePlus.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <iostream>
 #include <Windows.h>
-#include "ClearConsole.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "Color.h"
@@ -17,15 +16,21 @@ namespace console
 
 	void space();
 
-	void typingText(std::string message);
+	void typingText(std::string message, int delay);
 
 	void Epilepsy(std::string message);
 
 	void randNum(int var, int limit);
 
+	void loadingText(std::string message, int delay);
+
+	void print(std::string message);
+
+	void println(std::string message);
+
+
+
 	// void cin( std::string inputVar);
-
-
 	// void rainbowText(std::string message);
 
 
